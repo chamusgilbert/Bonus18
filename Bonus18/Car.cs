964a4cd3d10019b758e33538cc765e159600bd30
famouschamus <chamusg@gmail.com>
2018-02-05 16:26:20 -0500

    initial commit

---
+++ Bonus18/Car.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace Bonus18
+
+{
+    class Car
+    {
+        public string make;
+        public string model;
+        public int year;
+        public double price;
+        public string carInfo;
+
+        public Car(string make, string model, int year, double price)
+        {
+            this.make = make;
+            this.model = model;
+            this.year = year;
+            this.price = price;
+            this.carInfo = string.Format("{0,10}  {1,10}  {2,4}   {3,15}", make, model, year, price);
+        }
+
+        public static void Print(string info)
+        {
+            Console.WriteLine(info);
+        }
+
+
+    }
+    class UsedCar : Car
+    {
+
+        public UsedCar(string make, string model, int year, double price)
+            : base(make, model, year, price)
+        {
+
+        }
+    }
+}
---
+++ Bonus18/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Bonus18
+{
+    class Program
+    {
+        static void Main()
+        {
+            int c = 1, b = -1;
+            List<Car> carLot = MakeLot();
+            Console.WriteLine("Welcome to Grand Circus' Car Dealership!");
+            ViewLot(c, carLot);
+            b = BuyOne(carLot);
+            carLot.RemoveAt(b - 1);
+            ViewLot(c, carLot);
+            BuyMore(b, c, carLot);
+            Console.WriteLine("GoodBye!");
+
+        }
+
+        public static List<Car> MakeLot()
+        {
+            UsedCar car1 = new UsedCar("Toyota", "Prius", 2008, 10000);
+            UsedCar car2 = new UsedCar("Hyandai", "Santa Fe", 2007, 11200);
+            Car car3 = new Car("Chevy", "Cobalt", 2009, 45000);
+            Car car4 = new Car("Chevy", "Mustang", 2018, 99000);
+            Car car5 = new Car("Jeep", "Wrangler", 2015, 60600);
+            UsedCar car6 = new UsedCar("Chevrolet", "Cavalier", 2001, 2000);
+
+            List<Car> carLot = new List<Car>() { car1, car2, car3, car4, car5, car6 };
+
+            return carLot;
+        }
+        public static int BuyOne(List<Car> list)
+        {
+            Console.WriteLine("Pick a car to buy by it's number");
+            bool typeBetter = int.TryParse(Console.ReadLine(), out int buying);
+
+
+            while (typeBetter == false || buying > list.Count || buying < 1)
+            {
+                Console.WriteLine("We dont have a car that meets that number or discription");
+                Console.WriteLine("Pick a car to buy by it's number");
+                typeBetter = int.TryParse(Console.ReadLine(), out buying);
+            }
+            return buying;
+        }
+        public static void ViewLot(int c, List<Car> carLot)
+        {
+            Console.WriteLine("Take a look at our cars:");
+            foreach (Car item in carLot)
+            {
+
+                Console.WriteLine(c + item.carInfo);
+                c++;
+            }
+        }
+        public static void BuyMore(int b, int c, List<Car> carLot)
+        {
+            Console.WriteLine("Would you like to buy more?");
+            string reply = Console.ReadLine().ToLower();
+            if (carLot.Count == 0)
+            {
+                Console.WriteLine("Sorry, We are all out of cars");
+                return;
+            }
+            else if (reply == "y" || reply == "yes")
+            {
+                b = BuyOne(carLot);
+                carLot.RemoveAt(b - 1);
+                ViewLot(c, carLot);
+                BuyMore(b, c, carLot);
+            }
+            else if (reply == "n" || reply == "no")
+            {
+                Console.WriteLine("Thanks for your business");
+                return;
+
+            }
+            else
+            {
+                Console.WriteLine("I did not understand you, say again?");
+                BuyMore(b, c, carLot);
+            }
+        }
+    }
+}
