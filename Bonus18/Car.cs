ac42fb6ec6233cd64b57b1a193a31d2450f8cef2
famouschamus <chamusg@gmail.com>
2018-02-06 11:45:12 -0500

    Added UsedCars to it's own Class

--- Bonus18/Car.cs
+++ Bonus18/Car.cs
@@ -25,19 +25,5 @@
 
 
     }
-    class UsedCar : Car
-    {
-        public double miles;
-
-        public UsedCar(string make, string model, int year, double price, double miles) : base(make, model, year, price)
-        {
-            this.miles = miles;
-            this.carInfo = CarInfo(make, model, year, price, miles);
-        }
-        public static string CarInfo(string make, string model, int year, double price, double miles)
-        {
-            return string.Format("{0,10} {1,10} {2,4} {3,14} {4,15}", make, model, year, price, miles);
-        }
-
-    }
+   
 }
---
+++ Bonus18/UsedCar.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonus18
+{
+    class UsedCar:Car
+    {
+        public double miles;
+
+        public UsedCar(string make, string model, int year, double price, double miles) : base(make, model, year, price)
+        {
+            this.miles = miles;
+            this.carInfo = CarInfo(make, model, year, price, miles);
+        }
+        public static string CarInfo(string make, string model, int year, double price, double miles)
+        {
+            return string.Format("{0,10} {1,10} {2,4} {3,14} {4,15}", make, model, year, price, miles);
+        }
+    }
+}
