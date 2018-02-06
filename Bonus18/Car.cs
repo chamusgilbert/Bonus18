dad172df12056f3a8b5da5753967a3a926772035
famouschamus <chamusg@gmail.com>
2018-02-06 10:37:24 -0500

    Added Miles for UsedCar into the CarLot

--- Bonus18/Car.cs
+++ Bonus18/Car.cs
@@ -20,22 +20,23 @@
 
         public virtual string CarInfo(string make, string model, int year, double price)
         {
-            return string.Format("{0,10}  {1,10}  {2,4}   {3,15}", make, model, year, price);
+            return string.Format("{0,10}  {1,9} {2,4}   {3,12}", make, model, year, price);
         }
 
 
     }
     class UsedCar : Car
     {
+        public double miles;
 
-        public UsedCar(string make, string model, int year, double price, double miles)
-            : base(make, model, year, price)
+        public UsedCar(string make, string model, int year, double price, double miles) : base(make, model, year, price)
         {
-            CarInfo(make, model, year, price, miles);
+            this.miles = miles;
+            this.carInfo = CarInfo(make, model, year, price, miles);
         }
         public static string CarInfo(string make, string model, int year, double price, double miles)
         {
-            return string.Format("{0,10} {1,10} {2,4} {3,15} {4,15}", make, model, year, price, miles);
+            return string.Format("{0,10} {1,10} {2,4} {3,14} {4,15}", make, model, year, price, miles);
         }
 
     }
